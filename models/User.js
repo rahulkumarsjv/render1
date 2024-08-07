35e7b8e6f81d22e2b40c5638956566ae596c417e
rahulkumarsjv <156969736+rahulkumarsjv@users.noreply.github.com>
2024-08-08 00:17:31 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,39 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  PhoneNumber: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  walletBalance: { type: Number, default: 0 }
+// const userSchema = new mongoose.Schema({
+//   name: { type: String, required: true },
+//   PhoneNumber: { type: String, required: true },
+//   email: { type: String, required: true, unique: true },
+//   password: { type: String, required: true },
+//   walletBalance: { type: Number, default: 0 },
+// });
+
+// module.exports = mongoose.model('User', userSchema);
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  PhoneNumber: {
+     type: String, required: true 
+    },
+  email: {
+    type: String,
+    required: true
+  },
+  walletBalance: {
+    type: Number,
+    default: 0
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
+
