bd96b0206e998c5cc22177d1204ade981d814721
rahulkumarsjv <156969736+rahulkumarsjv@users.noreply.github.com>
2024-08-08 09:59:16 +0530

    Update PaymentAadharIndex.js

--- models/PaymentAadharIndex.js
+++ models/PaymentAadharIndex.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const paymentAadharSchema = new Schema({
+const userpaymentSchemar = new Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
