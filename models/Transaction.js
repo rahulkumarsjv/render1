53387a64e15f5be615ccbc7aeee9736a7b14fcff
rahulkumarsjv <156969736+rahulkumarsjv@users.noreply.github.com>
2024-08-21 03:48:58 +0530

    Update Transaction.js

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,28 +1,11 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const transactionSchema = new Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  amount: {
-    type: Number,
-    required: true
-  },
-  type: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
+const TransactionSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  amount: { type: Number, required: true },
+  type: { type: String, required: true }, // 'debit' or 'credit'
+  description: { type: String, required: true },
+  date: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('Transaction', transactionSchema);
+module.exports = mongoose.model('Transaction', TransactionSchema);
