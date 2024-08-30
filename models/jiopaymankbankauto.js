c34a98d2aa246d1b2ee25f6f8a2818c8e7b2612f
rahulkumarsjv <156969736+rahulkumarsjv@users.noreply.github.com>
2024-08-30 05:36:36 +0530

    Update jiopaymankbankauto.js

--- models/jiopaymankbankauto.js
+++ models/jiopaymankbankauto.js
@@ -1,23 +1,19 @@
-// models/Jiopaymankbank.js
 const mongoose = require('mongoose');
 
-const JiopaymankbankSchema = new mongoose.Schema({
+const jiopaymankbankSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     accountNo: { type: String, required: true },
     ifscCode: { type: String, required: true },
     name: { type: String, required: true },
     fatherName: { type: String, required: true },
-    date_of_birth: { type: String, required: true },
+    date_of_birth: { type: Date, required: true },
     address: { type: String, required: true },
     mobileNumber: { type: String, required: true },
     emailId: { type: String, required: true },
     branch: { type: String, required: true },
     branchAddress: { type: String, required: true },
-    accountOpenDate: { type: String, required: true },
-    uniqueNumber: { type: String, required: true, unique: true }
-    
+    accountOpenDate: { type: Date, required: true },
+    uniqueNumber: { type: String, required: true } // Ensure this is defined
 });
 
-const Jiopaymankbank = mongoose.model('Jiopaymankbank', JiopaymankbankSchema);
-
-module.exports = Jiopaymankbank;
+module.exports = mongoose.model('Jiopaymankbank', jiopaymankbankSchema);
